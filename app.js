e68b33ae2d7bc122ecd9cd90cad26cfd0cd86691
OneZerocococo <ylflyfly@gmail.com>
2022-10-09 21:25:32 +0800

    feat: add mongoose add restaurant seeder

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
-const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
+mongoose.connect(process.env.MONGODB_URI)
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-
 app.use(express.static('public'))
 
+
+// setting body-parser
+app.use(express.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantList.results })
+})
+app.post('/', (req, res) => {
+  console.log('req.body', req.body)
+  res.render('index')
 })
 
 app.get('/search', (req, res) => {
