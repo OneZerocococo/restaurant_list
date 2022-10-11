a89c6a6b61567edc76a303716a0c4470e0b4a307
OneZerocococo <ylflyfly@gmail.com>
2022-10-11 20:50:46 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose')
 const methodOverride = require('method-override')
-
+require('./config/mongoose')
 const routes = require('./routes')
+
 const app = express()
 const port = 3000
-const Restaurantlist = require('./models/restaurant')
-mongoose.connect(process.env.MONGODB_URI)
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
