80c2f46e20bba0c5d9d500b223a4f9ba6dbe607e
OneZerocococo <ylflyfly@gmail.com>
2022-11-04 16:43:02 +0800

    feat: add logout feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -7,10 +7,10 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
-router.post('/login', passport.authenticate('local'), {
+router.post('/login', passport.authenticate('local', {
   successRedirect: '/',
   failureRedirect: '/users/login'
-})
+}))
 
 router.get('/register', (req, res) => {
   res.render('register')
@@ -39,6 +39,13 @@ router.post('/register', (req, res) => {
       }
     })
     .catch(err => console.log(err))
+})
+
+router.get('/logout', (req, res, next) => {
+  req.logout(err => {
+    if (err) { return next(err) }
+    res.redirect('/users/login')
+  })
 })
 
 module.exports = router
